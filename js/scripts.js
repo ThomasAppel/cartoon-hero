a34da7fe55df30222f0c60acdd2185eb899845cc
thomasappel <tnappeldoorn@gmail.com>
2021-01-31 15:56:03 +0100

    A lot

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function openNav() {
+	document.getElementByld("mySidenav").style.transform = "translateX(0px)";
+	document.getElementByld("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+	document.getElementByld("mySidenav").style.transform = "translateX(-250px)";
+	document.getElementByld("main").style.marginLeft = "0px";
+}
